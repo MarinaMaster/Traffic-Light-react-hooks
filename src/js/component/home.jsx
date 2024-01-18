c1526d1848eb81525eff54f2259a342a901e4af7
Marina Myssak <marina.myssak@outlook.com>
2024-01-18 07:54:37 +0600

    complite traffic-light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [circleVariable, setCircleVariable] =useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-container">
+				<div className="container">
+					<div className="rectangle">
+						<div
+							className={"circle red" + ((circleVariable === "red") ? " light" : "")}
+							onClick={() => setCircleVariable("red")}
+						>.</div>
+						<div className={"circle yellow" + ((circleVariable === "yellow") ? " light" : "")}
+							 onClick={() => setCircleVariable("yellow")}
+						>.</div>
+
+						<div className={"circle green" + ((circleVariable === "green" ? " light" : ""))}
+							 onClick={() => setCircleVariable("green")}
+						>.</div>
+
+					</div>
+				</div>
+			<div className="stick">.</div>
 		</div>
 	);
 };
